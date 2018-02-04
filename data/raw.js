39b9ea4ef74c99b74da7a9ee8dcdab02954f87e8
Wendy <zhaod.wendy@gmail.com>
2018-02-04 15:23:00 -0800

    clean up data file

--- data/raw.js
+++ data/raw.js
@@ -1,2 +1,5 @@
-module.exports = [1.8, 1.6, 0.9, 1.2, 0.9,1.1, 0.5].map(function(x){return x*50})
+// Sample data
+let array = [1.8, 1.6, 0.9, 1.2, 0.9,1.1, 0.5];
+
+module.exports = array.map(function(x){return x*50});
 
